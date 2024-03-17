2bc09f5a262385612463e0ee909d93c382c59687
saitoi <pedrohhs@dcc.ufrj.br>
2024-03-17 01:52:10 -0300

    Full text search complete

--- app/models.py
+++ app/models.py
@@ -12,9 +12,45 @@ from time import time
 import jwt
 
 
-@login.user_loader
-def load_user(id):
-    return db.session.get(User, int(id))
+class SearchableMixin:
+    @classmethod
+    def search(cls, expression, page, per_page):
+        ids, total = query_index(cls.__tablename__, expression, page, per_page)
+        if total == 0:
+            return [], 0
+        when = [(id, i) for i, id in enumerate(ids)]
+        query = sa.select(cls).filter(cls.id.in_(ids)).order_by(
+            db.case(*when, value=cls.id))
+        return db.session.scalars(query), total
+        
+    @classmethod
+    def before_commit(cls, session):
+        session._changes = {
+            'add': list(session.new),
+            'update': list(session.dirty),
+            'delete': list(session.deleted),
+        }
+        
+    @classmethod
+    def after_commit(self, session):
+        for obj in session._changes['add']:
+            if isinstance(obj, SearchableMixin):
+                add_to_index(obj.__tablename__, obj)
+        for obj in session._changes['update']:
+            if isinstance(obj, SearchableMixin):
+                add_to_index(obj.__tablename__, obj)
+        for obj in session._changes['delete']:
+            if isinstance(obj, SearchableMixin):
+                remove_from_index(obj.__tablename__, obj)
+        session._changes = None
+        
+    @classmethod
+    def reindex(cls):
+        for obj in db.session.scalars(sa.select(cls)):
+            add_to_index(cls.__tablename__, obj)
+        
+db.event.listen(db.session, 'before_commit', SearchableMixin.before_commit)
+db.event.listen(db.session, 'after_commit', SearchableMixin.after_commit)
 
 followers = sa.Table(
     'followers',
@@ -113,46 +149,12 @@ class User(UserMixin, db.Model):
         except:
             return
         return db.session.get(User, id)
-    
-class SearchableMixin(object):
-    @classmethod
-    def search(cls, expression, page, per_page):
-        ids, total = query_index(cls.__tablename__, expression, page, per_page)
-        if total == 0:
-            return [], 0
-        when = [(id, i) for i, id in enumerate(ids)]
-        query = sa.select(cls).filter(cls.id.in_(ids)).order_by(
-            db.case(*when, value=cls.id))
-        return db.session.scalars(query), total
-        
-    @classmethod
-    def before_commit(cls, session):
-        session._changes = {
-            'add': list(session.new),
-            'update': list(session.dirty),
-            'delete': list(session.deleted),
-        }
-        
-    @classmethod
-    def after_commit(self, session):
-        for obj in session._changes['add']:
-            if isinstance(obj, SearchableMixin):
-                add_to_index(obj.__tablename__, obj)
-        for obj in session._changes['update']:
-            if isinstance(obj, SearchableMixin):
-                add_to_index(obj.__tablename__, obj)
-        for obj in session._changes['delete']:
-            if isinstance(obj, SearchableMixin):
-                remove_from_index(obj.__tablename__, obj)
-        session._changes = None
-        
-    @classmethod
-    def reindex(cls):
-        for obj in db.session.scalars(sa.select(cls)):
-            add_to_index(cls.__tablename__, obj)
-        
-db.event.listen(db.session, 'before_commit', SearchableMixin.before_commit)
-db.event.listen(db.session, 'after_commit', SearchableMixin.after_commit)
+
+
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
+
 
 class Post(SearchableMixin, db.Model):
     """
